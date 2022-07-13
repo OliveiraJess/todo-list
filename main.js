532d8b729badc89e078ab91494ef6118316a664a
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-13 16:06:11 -0300

    funcao inserir item para inserir novo item na lista atravesdo input, click item para verificar aonde foi clicado e remover item para remover item ao clicar em x

--- main.js
+++ main.js
@@ -4,17 +4,17 @@
 let banco = [
     { "tarefa": "estudar js", "status": "" },
     { "tarefa": "netflix", "status": "checked" },
-    { "tarefa": "react", "status": "checked" }
+    { "tarefa": "react", "status": "" }
 ]
 
 //criar item 
-const criarItem = (tarefa, status) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement("label");
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox" ${status}>
+    <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
-    <input type="button" value="X">
+    <input type="button" value="X" data-indice=${indice}>
     `
     document.getElementById('todoList').appendChild(item);
 }
@@ -31,7 +31,47 @@ const limparTarefas = () => {
 //atualizar tela conforme o banco de dados 
 const atualizarTela = () => {
     limparTarefas();
-    banco.forEach(item => criarItem(item.tarefa, item.status));
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 }
+
+
+//inserir item no banco e atualizar a tela
+const inserirItem = (evento) => {
+    //verifica a tecla 
+    const tecla = evento.key;
+    //pega oq foi digitado no input
+    const texto = evento.target.value;
+
+    if (tecla === "Enter") {
+        banco.push({ "tarefa": texto, "status": "" })
+        atualizarTela();
+        //limpar texto do input
+        evento.target.value = "";
+    }
+
+}
+
+//remover item
+const removerItem = (indice) => {
+    //recorta/modifica o array
+    banco.splice(indice, 1);
+    atualizarTela();
+}
+
+//verificar click no item e remover
+const clickItem = (evento) => {
+    const elemento = evento.target
+
+    if (elemento.type === "button") {
+        const indice = elemento.dataset.indice
+        removerItem(indice);
+    }
+}
+
+
+//acrescentar tarefa no banco
+document.getElementById("newItem").addEventListener('keypress', inserirItem)
+//pegar click no item
+document.getElementById("todoList").addEventListener('click', clickItem)
 
 atualizarTela();
