9bb0b9ee5918a5ef9b0129f92ba5706cb063b8b3
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-13 14:43:33 -0300

    passando parametros ao criar item

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const criarItem = () => {
+const criarItem = (tarefa, status="") => {
     const item = document.createElement("label");
     item.classList.add('todo__item');
     item.innerHTML = `
-    <input type="checkbox">
-    <div>Teste de item 1</div>
+    <input type="checkbox" ${status}>
+    <div>${tarefa}</div>
     <input type="button" value="X">
     `
     document.getElementById('todoList').appendChild(item);
