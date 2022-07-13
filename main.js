52322dec6fcccd7d5c3b7a55b612883d1203928f
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-07-13 15:00:01 -0300

    atualizando a tela conforme banco de dados e limpando tarefas

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-const criarItem = (tarefa, status="") => {
+'use strict'
+
+//banco de dados
+let banco = [
+    { "tarefa": "estudar js", "status": "" },
+    { "tarefa": "netflix", "status": "checked" },
+    { "tarefa": "react", "status": "checked" }
+]
+
+//criar item 
+const criarItem = (tarefa, status) => {
     const item = document.createElement("label");
     item.classList.add('todo__item');
     item.innerHTML = `
@@ -8,3 +18,20 @@ const criarItem = (tarefa, status="") => {
     `
     document.getElementById('todoList').appendChild(item);
 }
+
+//limpar tarefas
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild)
+    }
+}
+
+//atualizar tela conforme o banco de dados 
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+}
+
+atualizarTela();
